981dfc9d528a6705ddb75246f023a90346be1114
leo <algo2000@naver.com>
2021-04-27 23:59:21 +0900

    update implemented selector

--- recoil-next-app-concept/components/FontButton.tsx
+++ recoil-next-app-concept/components/FontButton.tsx
@@ -1,13 +1,18 @@
-import { useRecoilState } from "recoil";
-import fontSizeState from "./fontSizeState";
+import { useRecoilState, useRecoilValue } from "recoil";
+import fontSizeState, { fontSizeLabelState } from "./fontSizeState";
 
 const FontButton = () => {
-	const [fontSize, setFontSize] = useRecoilState(fontSizeState)
-	return(
-		<button onClick={() => setFontSize((size) => size + 1)} style={{fontSize}}>
-      Click to Enlarge
+  const [fontSize, setFontSize] = useRecoilState(fontSizeState)
+  const fontSizeLabel = useRecoilValue(fontSizeLabelState);
+
+  return (
+    <>
+      <button onClick={() => setFontSize((size) => size + 1)} style={{ fontSize }}>
+        Click to Enlarge
     </button>
-	)
+      <div>Current font size: ${fontSizeLabel}</div>
+    </>
+  )
 }
 
 export default FontButton;
--- recoil-next-app-concept/components/fontSizeState.tsx
+++ recoil-next-app-concept/components/fontSizeState.tsx
@@ -1,8 +1,18 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 const fontSizeState = atom({
 	key: 'fontSizeState',
 	default: 14,
 })
+
+export const fontSizeLabelState = selector({
+	key: 'fontSizeLabelState',
+	get: ({get}) => {
+		const fontSize = get(fontSizeState);
+    const unit = 'px';
+
+    return `${fontSize}${unit}`;
+	},
+});
 
 export default fontSizeState;
