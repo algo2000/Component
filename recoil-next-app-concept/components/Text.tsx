19590566b952094d9733a0247ac22ee17ac1111b
leo <algo2000@naver.com>
2021-04-28 00:00:31 +0900

    fixed warning

--- recoil-next-app-concept/components/Text.tsx
+++ recoil-next-app-concept/components/Text.tsx
@@ -2,7 +2,7 @@ import { useRecoilState } from "recoil";
 import fontSizeState from "./fontSizeState";
 
 const Text = () => {
-  const [fontSize, setFontSize] = useRecoilState(fontSizeState);
+  const [fontSize] = useRecoilState(fontSizeState);
   return <p style={{fontSize}}>This text will increase in size too.</p>;
 }
 
