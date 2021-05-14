b412e3a4c284eec3418d7b170d095d9119d791ae
leo <algo2000@naver.com>
2021-05-14 23:40:03 +0900

    .

--- belf-component/pages/about.tsx
+++ belf-component/pages/about.tsx
@@ -1,21 +1,9 @@
 import Link from 'next/link'
 import React from 'react'
-import { MotionStyle } from '.'
 
 const AboutPage = () => (
-  <div>
-    <MotionStyle
-      animate={{
-        borderRadius: ["20%", "100%"]
-      }}
-      transition={{
-        times: [0.5],
-      }}
-    />
-    <Link href="/">
-      <a>Link To home</a>
-    </Link>
-  </div>
+  <>
+  </>
 )
 
 export default AboutPage
